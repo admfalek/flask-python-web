f98d4edec14183d9c71529ad01269c2697bc8bad
Adam Fałek <a.falek2@tme.pl>
2018-06-11 14:03:32 +0200

    flask

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flask + Bootstrap = python web starter kit
+
+Webapp starter pack with python, based on Flask, Jinja templates and Bootstrap.
+Run on localhost, by:
+```
+python app.py
+```
---
+++ data.py
@@ -0,0 +1,20 @@
+def Blog():
+	blog = [
+		{
+			'id': 1,
+			'title': 'First Article',
+			'content': 'Lorem ipsu dolor sit amet. Article content.',
+			'permalink': 'first-article',
+			'author': 'John Bold',
+			'create_date': '04-25-2018'
+		},
+		{
+			'id': 2,
+			'title': 'Second Article',
+			'content': 'Next article on our blog. Lorem ipsu dolor sit amet. Article content two.',
+			'permalink': 'second-article',
+			'author': 'Tom Lin',
+			'create_date': '05-07-2018'
+		}
+	]
+	return blog
